eb2fbbdfbaf076729699918edf0a78c0191af770
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-10-30 19:06:14 +0600

    Hero Text Added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,13 +7,60 @@ const Home = () => {
     <div className="bg-NeutralSilver">
       <div className=" px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
         <Carousel className=" w-full mx-auto">
+          {/* First Carousel */}
           <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
             <div>
               <img src={banner1} alt="" />
             </div>
+            {/* Hero Text over Carousel Section  */}
+            <div className="md:w-1/2">
+              <h1 className="text-5xl font-semibold mb-4 text-NeutralD_Grey md:w-3/4 leading-snug">
+                Lessons and insights{" "}
+                <span className="text-BrandPrimary">from 8 years</span>
+              </h1>
+              <p className="text-NeutralD_Grey text-base mb-8 ">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="btn-primary"> Register</button>
+            </div>
           </div>
-          <div className="flex h-full items-center justify-center">Slide 2</div>
-          <div className="flex h-full items-center justify-center">Slide 3</div>
+          {/* second Carousel */}
+          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+            <div>
+              <img src={banner1} alt="" />
+            </div>
+            {/* Hero Text over Carousel Section  */}
+            <div className="md:w-1/2">
+              <h1 className="text-5xl font-semibold mb-4 text-NeutralD_Grey md:w-3/4 leading-snug">
+                Lessons and insights{" "}
+                <span className="text-BrandPrimary">from 8 years</span>
+              </h1>
+              <p className="text-NeutralD_Grey text-base mb-8 ">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="btn-primary"> Register</button>
+            </div>
+          </div>
+          {/* Third Carousel */}
+          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12">
+            <div>
+              <img src={banner1} alt="" />
+            </div>
+            {/* Hero Text over Carousel Section  */}
+            <div className="md:w-1/2">
+              <h1 className="text-5xl font-semibold mb-4 text-NeutralD_Grey md:w-3/4 leading-snug">
+                Lessons and insights{" "}
+                <span className="text-BrandPrimary">from 8 years</span>
+              </h1>
+              <p className="text-NeutralD_Grey text-base mb-8 ">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="btn-primary"> Register</button>
+            </div>
+          </div>
         </Carousel>
       </div>
     </div>
