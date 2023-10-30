b256f1e890939a779af517a4e0d2c1d45577125e
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-10-30 19:17:43 +0600

    Services Component Added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Services />
+    </>
+  );
+}
+
+export default App;
