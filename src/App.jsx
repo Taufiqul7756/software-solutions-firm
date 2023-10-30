40a7787c7e273b3c88ef820930ac9f9de92dc5ae
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-10-30 21:23:09 +0600

    Client component Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Services from "./components/Services";
+import Client from "./components/Client";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Services />
+      <Client />
     </>
   );
 }
---
+++ src/components/Client.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaApple } from "react-icons/fa6";
+// import intel from "../assets/icons/intel.ico";
+// import ipsum from "../assets/icons/ipsum.png";
+// import client2 from "../assets/icons/client 2.png";
+// import client3 from "../assets/icons/client 3.png";
+// import client4 from "../assets/icons/client 4.png";
+// import client6 from "../assets/icons/client 6.png";
+// import client7 from "../assets/icons/client 7.png";
+// import client8 from "../assets/icons/client 8.png";
+
+import sepa from "../assets/icons/sepa.png";
+import yolo from "../assets/icons/yolo.png";
+// import notion from "../assets/icons/notion.svg";
+import redux from "../assets/icons/redux.svg";
+import random1 from "../assets/icons/random1.svg";
+import random2 from "../assets/icons/random2.svg";
+import ipsum from "../assets/icons/ipsum.png";
+import uberEats from "../assets/icons/uberEats.svg";
+
+const Client = () => {
+  return (
+    <div className="md:px-14 px-4 py-16 max-w-screen-2xl mx-auto ">
+      <div className="text-center my-8 ">
+        <h2 className=" text-4xl text-NeutralD_Grey font-semibold mb-2">
+          {" "}
+          Our Clients
+        </h2>
+        <p className="text-4xl text-NeutralGrey">
+          We have been working with some Fortune 500+ clients
+        </p>
+        <div className="my-12 flex flex-wrap justify-between items-center gap-28">
+          <img src={ipsum} alt="" />
+          <img src={sepa} alt="" />
+          <img src={yolo} alt="" />
+          <img src={uberEats} alt="" />
+          <img src={redux} alt="" />
+          <img src={random1} alt="" />
+          <img src={random2} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Client;
