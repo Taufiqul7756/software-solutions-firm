555e14cadc1361d15302814c7fbf9cbcd6830803
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-10-30 13:55:33 +0600

    Colors setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        NeutralD_Grey: "#4D4D4D",
+        NeutralGrey: "#717171",
+        NeutralSilver: "#F5F7FA",
+        BrandPrimary: "#4CAF4F",
+      },
+    },
   },
   plugins: [],
 };
