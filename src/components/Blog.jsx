a57449170c4c5da7f8ccc845c3b19fb1caddb535
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-10-31 21:22:47 +0600

    Blogs Updated

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -32,29 +32,35 @@ const Blog = () => {
     <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-12">
       <div className="text-center md:w-1/2 mx-auto ">
         <h2 className="text-4xl text-NeutralD_Grey font-semibold mb-4">
-          INTELLIGENT BUSINESS OPERATOR SYSTEM-{" "}
-          <span className="text-BrandPrimary">weMoney</span>
+          LATEST BLOGS- <span className="text-BrandPrimary">weStrike</span>
         </h2>
-        <h3 className="text-xl text-NeutralD_Grey font-semibold mb-4 md:w-4/5">
-          A highly office solution for your business
-        </h3>
+        {/* <h3 className="text-xl text-NeutralD_Grey font-semibold mb-4 md:w-4/5">
+          LATEST BLOGS
+        </h3> */}
         <p className=" text-sm text-NeutralGrey mb-8 md:w-3/4 mx-auto">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
           justo ipsum. Sed accumsan quam vitae est varius fringilla.
-          Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
-          tristique iaculis. Nullam pulvinar sit amet risus pretium auctor.
-          Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec
-          elementum pulvinar odio.
+          Pellentesque p
         </p>
       </div>
       {/* all blogs here */}
       <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 items-center justify-between">
         {blogs.map((blog) => (
           <div key="blog.id" className="mx-auto relative mb-12 cursor-pointer">
-            <img src={blog.image} alt="" />
-            <div className="text-center px=4 py-8 bg-white shadow-lg rounded-md md:w-3/4 mx-auto absolute left-0 right-0 bottom-12">
-              <h3>{blog.title}</h3>
-              <div className="flex items-center justify-center gap- 8">
+            <img
+              src={blog.image}
+              alt=""
+              className="hover:scale-95 transition-all duration-300 "
+            />
+            <div
+              style={{ margin: "81px 0px 0px 43px" }}
+              className="text-center px-4 py-8 bg-white shadow-lg rounded-md md:w-3/4 mx-auto absolute left-0 right-0 top-12"
+            >
+              <h3 className="mb-3 text-NeutralGrey font-semibold ">
+                {blog.title}
+              </h3>
+              <p>{blog.description}</p>
+              <div className="flex items-center justify-center gap-8">
                 <a href="/">
                   {" "}
                   <span className="text-BrandPrimary text-xm font-bold hover:text-neutral-700">
